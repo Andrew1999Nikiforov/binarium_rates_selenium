852dcdaf13538fdc0e4d899635b41272a5013971
Andrew1999Nikiforov <niki.andrew1999@mail.ru>
2023-08-05 20:38:40 +0300

    Реализация функции по проверке времени. Какой промежуток выбирать в активах 1-5 мин или 5+

--- work_site.py
+++ work_site.py
@@ -13,9 +13,21 @@ def change_real_money_to_game(driver): # Перевод счета с реаль
     except TimeoutException:
         print("Кнопка для перевода счета с реального на игровой не найдена за отведенное время.")
 
+def is_time_difference_greater_than_5_minutes(time_str1, time_str2): # сравнение двух времени 
+    time_format = '%H:%M'
+    time1 = datetime.strptime(time_str1, time_format)
+    time2 = datetime.strptime(time_str2, time_format)
+    time_diff_seconds = abs((time2 - time1).total_seconds())
+    return time_diff_seconds > 5 * 60
+
 def change_long_or_short_active(): # выбор % в активах
-    now = datetime.datetime.now()
-    if
+    current_time = datetime.now().strftime('%H:%M')
+    if is_time_difference_greater_than_5_minutes(current_time, "20:41"):
+        return True
+    else:
+        return False
+            
+change_long_or_short_active()
 
 def change_active_money(driver): # выбор активов слева сверху
     try:
