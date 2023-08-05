7729fb446f771d7cc348754e59f4b4cc76f0d299
Andrew1999Nikiforov <niki.andrew1999@mail.ru>
2023-08-05 15:57:14 +0300

    Файл для вызова освновных функций например входа в аккаунт, смена счетов, выбор денег

---
+++ binarium_sites_pars.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+import login_site
+import work_site
+import time
+
+driver = webdriver.Chrome() 
+url = "https://binarium.win/ru/terminal"  
+
+driver.get(url)
+
+login_site.login(driver)
+work_site.change_real_money_to_game(driver)
+
+driver.quit()
