8b3e52dcc3f83d3ef63175209fdff53113b73c07
Andrew1999Nikiforov <niki.andrew1999@mail.ru>
2023-08-06 10:42:14 +0300

    Реализация сервера отвечающия за приемь сообщений

--- binarium_sites_pars.py
+++ binarium_sites_pars.py
@@ -5,11 +5,16 @@ import work_site
 driver = webdriver.Chrome() 
 url = "https://binarium.win/ru/terminal"  
 
-driver.get(url)
+if __name__ == "__main__":
+    driver.get(url)
+    login_site.login(driver)
+    work_site.change_real_money_to_game(driver)
 
-login_site.login(driver)
-work_site.change_real_money_to_game(driver)
-work_site.change_active_money(driver)
-work_site.change_time(driver)
-work_site.change_up_or_down(driver)
+while True:
+    work_site.start_program_y()
+    work_site.change_active_money(driver)
+    work_site.change_time(driver)
+    work_site.change_up_or_down(driver)
+    
 driver.quit()
+
--- work_site.py
+++ work_site.py
@@ -5,6 +5,16 @@ from selenium.common.exceptions import TimeoutException
 from password import param
 import time
 from datetime import datetime
+import socket
+
+def start_program_y(): # Получаем строку из второй программы
+    server_address = ('localhost', 14777)  # Укажите адрес и порт, на котором программа Y будет слушать
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        s.bind(server_address)
+        print("Ждем сообщение...")
+        data, address = s.recvfrom(1024)
+        param.text_sms = data.decode('utf-8')
+        print("Сообщение от программы X:", param.text_sms)
 
 def change_real_money_to_game(driver): # Перевод счета с реального на игровой
     try: 
@@ -33,7 +43,7 @@ def change_active_money(driver): # выбор активов слева свер
         input_active = WebDriverWait(driver, param.timeout).until(EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Поиск актива"]')))
         input_active.send_keys("LATAM")
         time.sleep(1)
-        if change_long_or_short_active("22:16"):
+        if change_long_or_short_active("08:27"):
             elements = WebDriverWait(driver, param.timeout).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'asset-profit__score')))
             second_element = elements[1]
             second_element.click()
@@ -45,20 +55,20 @@ def change_active_money(driver): # выбор активов слева свер
 def change_time(driver): # Установка времени 
     try:
         text_from_time = WebDriverWait(driver, param.timeout).until(EC.presence_of_element_located((By.CSS_SELECTOR, '[a-test="currentExpiration"]'))).get_attribute("title")
-        while text_from_time < "22:16":
+        while text_from_time < "08:27":
             WebDriverWait(driver, param.timeout).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.t-expiration-spinners .spinners__button.--inc'))).click()
             time.sleep(1)
             text_from_time = WebDriverWait(driver, param.timeout).until(EC.presence_of_element_located((By.CSS_SELECTOR, '[a-test="currentExpiration"]'))).get_attribute("title")
     except TimeoutException:
         print("Кнопка времени не найдена")
 
-def change_up_or_down(driver):
+def change_up_or_down(driver): # 
     try:
-        time.sleep(5)
+        time.sleep(2)
         if "up":
             WebDriverWait(driver, param.timeout).until(EC.presence_of_element_located((By.CLASS_NAME, '--call'))).click()
         else:
             WebDriverWait(driver, param.timeout).until(EC.presence_of_element_located((By.CLASS_NAME, '--put'))).click()
-        time.sleep(360)
+        time.sleep(30)
     except TimeoutException:
         print("Кнопка времени не найдена")
