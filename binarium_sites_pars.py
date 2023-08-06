09d02e071a7f1e056d3170b72dbda01d09647203
Andrew1999Nikiforov <niki.andrew1999@mail.ru>
2023-08-06 12:21:48 +0300

    Самая главный файл, который запускает все остальное

--- binarium_sites_pars.py
+++ binarium_sites_pars.py
@@ -1,20 +1,12 @@
 from selenium import webdriver
 import login_site
 import work_site
+import password
+import manage_prog
 
 driver = webdriver.Chrome() 
-url = "https://binarium.win/ru/terminal"  
 
-if __name__ == "__main__":
-    driver.get(url)
-    login_site.login(driver)
-    work_site.change_real_money_to_game(driver)
+manage_prog.manage_programm_function(driver)
 
-while True:
-    work_site.start_program_y()
-    work_site.change_active_money(driver)
-    work_site.change_time(driver)
-    work_site.change_up_or_down(driver)
-    
 driver.quit()
 
