9023568196aab5943d450e8be0bb8e376eb6fb2b
Andrew1999Nikiforov <niki.andrew1999@mail.ru>
2023-08-09 13:09:07 +0300

    Реализована функция которая добавляет знак / в слова из словаря active из файла password

--- work_site.py
+++ work_site.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from datetime import datetime
 from password import param
+from password import active
 import socket
 import re
 
@@ -22,7 +23,8 @@ def add_slash(input_string):
     return input_string.replace(" ", "/")
 
 def text_processing(message): # Функция которая обрабатывает строку с канала
-    pattern = r'([\w\s\(\)]+)\s+((?:вверх|вниз))\s+(\d{2}:\d{2})'
+    pattern = r'([\w\s\(\)]+)\s+((?:вверх|вниз|ВВЕРХ|ВНИЗ))\s+(\d{2}:\d{2})'
+    message = check_name_active(message)
     message = remove_slash(message)
     match = re.match(pattern, message)
     if match:
@@ -33,6 +35,11 @@ def text_processing(message): # Функция которая обрабатыв
         return True
     else:
         return False
+
+def check_name_active(text_without_slash): # Функция которая добавляет знак / в слова из словаря active
+    if text_without_slash in active.active:
+        return(text_without_slash[:3] + '/' + text_without_slash[3:])
+
 
 def change_real_money_to_game(driver): # Перевод счета с реального на игровой
     try: 
